d44ec9c79d49c2702a06ddc287e77665fbf17374
siddhartha8916 <siddhartha8916@gmail.com>
2023-06-12 23:28:23 +0530

    add navbar and hero section

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">OpenAI ChatBot</a>
+        <Link className="navbar-brand" href="/">
+          OpenAI ChatBot
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -23,7 +26,7 @@ const Navbar = () => {
               <ul className="dropdown-menu">
                 <li><a className="dropdown-item" href="#">Action</a></li>
                 <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><hr className="dropdown-divider"/></li>
+                <li><hr className="dropdown-divider" /></li>
                 <li><a className="dropdown-item" href="#">Something else here</a></li>
               </ul>
             </li>
